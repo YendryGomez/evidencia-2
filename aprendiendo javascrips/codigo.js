321bcd40c20d9c76fbcd7a1d9a1d3bc57ca66a9b
YendryGomez <20210992@itla.edu.do>
2026-02-26 13:57:14 -0400

    Add files via upload

---
+++ aprendiendo javascrips/codigo.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("click", function (e) {
+	if (e.target.classList.contains("gallery-item")){
+		const src = e.target.getAttribute("src");
+		document.querySelector(".modal-img").src = src;
+
+		const myModal = new bootstrap.modal(
+			document.getElementById("gallery-modal"));
+		myModal.show();
+	}
+});
